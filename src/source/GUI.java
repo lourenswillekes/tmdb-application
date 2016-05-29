13123122da16d69549e5ef382b922101c314f137
caleb hayward <caleb hayward>
2016-05-29 14:26:01 -0400

    Made changes to the GUI

--- src/source/GUI.java
+++ src/source/GUI.java
@@ -12,6 +12,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.RescaleOp;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.awt.event.ActionEvent;
 import java.awt.Label;
 import java.awt.Font;
@@ -26,14 +27,37 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
+import info.movito.themoviedbapi.TmdbApi;
+import info.movito.themoviedbapi.TmdbMovies;
+import info.movito.themoviedbapi.model.MovieDb;
+
 import javax.swing.ListSelectionModel;
 
 public class GUI {
 
-	private JFrame frame;
-	private JTable tblMovieList;
-	private DefaultTableCellRenderer inboxTableCellRenderer = new DefaultTableCellRenderer();
+	private static JFrame frame;
 	private SearchGUI x;
+	private static TmdbApi tmdbApi;
+	
+	static JLabel lblTab;
+	static JButton btnSelectRandom;
+	static JButton btnAddFav;
+	static JTextPane txtPlotDiscription;
+	static JButton btnAddWatch;
+	static JLabel lblMoviePoster;
+	static JScrollPane spPlotDiscription;
+	static JScrollPane spMovieInfo;
+	static JTextPane txtMovieInfo;
+	static JToggleButton tglbtnUpcoming;
+	static JToggleButton tglbtnPopular;
+	static JToggleButton tglbtnTopRated;
+	static JToggleButton tglbtnSearch;
+	static JTable tblMovieList;
+	static JToggleButton tglbtnNowPlaying;
+	static JScrollPane spMovieList;
+	static JLabel lblMovieInfo;
+	static JLabel lblPlotDiscription;
+	
 
 	/**
 	 * Launch the application.
@@ -42,8 +66,9 @@ public class GUI {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GUI window = new GUI();
-					window.frame.setVisible(true);
+					tmdbApi = new TmdbApi("34b0b2ee2ac7865db7bd356da1221847");
+					new GUI();
+					GUI.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -53,146 +78,69 @@ public class GUI {
 
 	/**
 	 * Create the application.
+	 * @throws IOException 
 	 */
-	public GUI() {
+	public GUI() throws IOException {
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @throws IOException 
 	 */
-	private void initialize() {
+	private void initialize() throws IOException {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 943, 545);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		///*
-		BufferedImage img = null;
-		try {
-		    img = ImageIO.read(new File("E:\\CIS 350\\MovieAPI\\junglebookbackdrop.jpg"));
-		} catch (IOException e) {
-		    e.printStackTrace();
-		}
+		CreateGUI();
+		displayMovieInfo(100);
 		
-		RescaleOp rescaleOp = new RescaleOp(1.2f, 150, null);
-		rescaleOp.filter(img, img);
-		
-		Image dimg = img.getScaledInstance(943, 580, Image.SCALE_SMOOTH);
-		
-		ImageIcon imageIcon = new ImageIcon(dimg);
-		
-		
-		JLabel label = new JLabel(imageIcon);
-		frame.setContentPane(label);
-		
-		//*/
-		
-		JLabel lblTab = new JLabel("");
-		lblTab.setFont(new Font("Cooper Black", Font.PLAIN, 20));
-		lblTab.setBounds(10, 60, 356, 29);
-		frame.getContentPane().add(lblTab);
-		
-		JButton btnSelectRandom = new JButton("Select Random");
-		btnSelectRandom.setBounds(353, 453, 164, 32);
 		btnSelectRandom.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 			}
 		});
-		btnSelectRandom.setFont(new Font("Modern No. 20", Font.BOLD, 14));
-		frame.getContentPane().add(btnSelectRandom);
 		
-		JButton btnAddFav = new JButton("Add To Favorites");
-		btnAddFav.setBounds(527, 453, 170, 32);
 		btnAddFav.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnAddFav.setFont(new Font("Modern No. 20", Font.BOLD, 14));
-		frame.getContentPane().add(btnAddFav);
 		
-		JButton btnAddWatch = new JButton("Add To Watchlist");
-		btnAddWatch.setBounds(733, 453, 170, 32);
+		
 		btnAddWatch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 			}
 		});
-		btnAddWatch.setFont(new Font("Modern No. 20", Font.BOLD, 14));
-		frame.getContentPane().add(btnAddWatch);
 		
-		JLabel lblPoster_Image = new JLabel("");
-		lblPoster_Image.setIcon(new ImageIcon("E:\\CIS 350\\MovieAPI\\junglebook.jpg"));
-		lblPoster_Image.setBounds(528, 39, 185, 278);
-		frame.getContentPane().add(lblPoster_Image);
-		
-		JLabel lblMoviePoster = new JLabel("Movie Poster");
-		lblMoviePoster.setForeground(Color.BLACK);
-		lblMoviePoster.setBounds(529, 11, 168, 25);
-		lblMoviePoster.setFont(new Font("Cooper Black", Font.BOLD, 20));
-		frame.getContentPane().add(lblMoviePoster);
-		
-		JScrollPane spPlotDiscription = new JScrollPane();
-		spPlotDiscription.setBounds(527, 344, 374, 98);
-		frame.getContentPane().add(spPlotDiscription);
-		
-		JTextPane txtPlotDiscription = new JTextPane();
-		txtPlotDiscription.setBackground(new Color(65, 105, 225));
-		txtPlotDiscription.setFont(new Font("Tahoma", Font.BOLD, 14));
-		txtPlotDiscription.setOpaque(false);
-		txtPlotDiscription.setForeground(Color.BLACK);
-		spPlotDiscription.setViewportView(txtPlotDiscription);
-		spPlotDiscription.setOpaque(false);
-		spPlotDiscription.getViewport().setOpaque(false);
-		
-		JScrollPane spMovieInfo = new JScrollPane();
-		spMovieInfo.setBounds(723, 39, 180, 276);
-		frame.getContentPane().add(spMovieInfo);
-		
-		JTextPane txtMovieInfo = new JTextPane();
-		txtMovieInfo.setBackground(new Color(65, 105, 225));
-		txtMovieInfo.setFont(new Font("Tahoma", Font.BOLD, 14));
-		txtMovieInfo.setOpaque(false);
-		txtMovieInfo.setForeground(Color.BLACK);
-		spMovieInfo.setViewportView(txtMovieInfo);
-		spMovieInfo.setOpaque(false);
-		spMovieInfo.getViewport().setOpaque(false);
-		
-		JToggleButton tglbtnUpcoming = new JToggleButton("Upcoming");
-		tglbtnUpcoming.setBounds(196, 0, 100, 32);
-		tglbtnUpcoming.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		tglbtnUpcoming.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 				lblTab.setText("Top 20 Upcoming Films");
 			}
 		});
-		frame.getContentPane().add(tglbtnUpcoming);
 		
-		JToggleButton tglbtnPopular = new JToggleButton("Popular");
-		tglbtnPopular.setBounds(0, 0, 100, 32);
-		tglbtnPopular.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		tglbtnPopular.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				try {
+					displayMovieInfo(101);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				lblTab.setText("Top 20 Popular Films");
 			}
 		});
-		frame.getContentPane().add(tglbtnPopular);
 		
-		JToggleButton tglbtnTopRated = new JToggleButton("Top Rated");
-		tglbtnTopRated.setBounds(99, 0, 100, 32);
-		tglbtnTopRated.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		tglbtnTopRated.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				lblTab.setText("Top 20 Top Rated Films");
 			}
 		});
-		frame.getContentPane().add(tglbtnTopRated);
 		
-		JToggleButton tglbtnSearch = new JToggleButton("Search");
-		tglbtnSearch.setBounds(393, 0, 100, 32);
-		tglbtnSearch.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		tglbtnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -205,19 +153,157 @@ public class GUI {
 				//lblTab.setText("Top 20 Searched Results");
 			}
 		});
-		frame.getContentPane().add(tglbtnSearch);
 		
-		JToggleButton tglbtnNowPlaying = new JToggleButton("Now Playing");
-		tglbtnNowPlaying.setBounds(295, 0, 100, 32);
-		tglbtnNowPlaying.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		tglbtnNowPlaying.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				lblTab.setText("Top 20 Now Playing Films");
 			}
 		});
+		
+		//Add action listener for tabel
+	}
+	
+	public static void setMoviePoster(String posterPath) throws IOException {
+		
+		URL posterURL = new URL(posterPath);
+		
+		BufferedImage poster = ImageIO.read(posterURL);
+	    
+		Image scaledPoster = poster.getScaledInstance(180, 276, Image.SCALE_SMOOTH);
+		
+    	JLabel lblMoviePoster = new JLabel(new ImageIcon(scaledPoster));
+	    
+	   	lblMoviePoster.setBounds(528, 39, 185, 278);
+	    
+	   	frame.getContentPane().add(lblMoviePoster); 	
+	}
+	
+	private static void displayMovieInfo(int movieID) throws IOException {
+		
+		TmdbMovies tmdbMovies = tmdbApi.getMovies();
+		
+		// Retrieve information on a movie given movie id
+		MovieDb movie = tmdbMovies.getMovie(movieID, "en");
+		
+		// Display movie poster
+		String backdropPath = "https://image.tmdb.org/t/p/original" + movie.getBackdropPath();
+		try {
+			setMovieBackdrop(backdropPath);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Display movie backdrop
+		String posterPath = "https://image.tmdb.org/t/p/original" + movie.getPosterPath();
+		try {
+			setMoviePoster(posterPath);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		CreateGUI();
+		txtPlotDiscription.setText(movie.getOverview());
+		
+	}
+	
+	public static void setMovieBackdrop(String backdropPath) throws IOException {
+		URL backdropURL = new URL(backdropPath);
+		
+		BufferedImage backdrop = ImageIO.read(backdropURL);
+	    
+		RescaleOp rescaleOp = new RescaleOp(1.2f, 150, null);
+		rescaleOp.filter(backdrop, backdrop);
+		
+		Image scaledBackdrop = backdrop.getScaledInstance(943, 545, Image.SCALE_SMOOTH);
+		
+	    JLabel lblBackdrop = new JLabel(new ImageIcon(scaledBackdrop));
+	    
+	    frame.setContentPane(lblBackdrop);  
+	}
+	
+	public static void CreateGUI() throws IOException {
+		
+		lblTab = new JLabel("");
+		lblTab.setFont(new Font("Cooper Black", Font.PLAIN, 20));
+		lblTab.setBounds(10, 60, 356, 29);
+		frame.getContentPane().add(lblTab);
+	
+		btnSelectRandom = new JButton("Select Random");
+		btnSelectRandom.setBounds(353, 453, 164, 32);
+		btnSelectRandom.setFont(new Font("Modern No. 20", Font.BOLD, 14));
+		frame.getContentPane().add(btnSelectRandom);
+		
+		btnAddFav = new JButton("Add To Favorites");
+		btnAddFav.setBounds(527, 453, 170, 32);
+		btnAddFav.setFont(new Font("Modern No. 20", Font.BOLD, 14));
+		frame.getContentPane().add(btnAddFav);
+		
+		spPlotDiscription = new JScrollPane();
+		spPlotDiscription.setBounds(527, 344, 374, 98);
+		frame.getContentPane().add(spPlotDiscription);
+		
+		txtPlotDiscription = new JTextPane();
+		txtPlotDiscription.setBackground(new Color(65, 105, 225));
+		txtPlotDiscription.setFont(new Font("Tahoma", Font.BOLD, 14));
+		txtPlotDiscription.setOpaque(false);
+		txtPlotDiscription.setForeground(Color.BLACK);
+		spPlotDiscription.setViewportView(txtPlotDiscription);
+		spPlotDiscription.setOpaque(false);
+		spPlotDiscription.getViewport().setOpaque(false);
+		
+		btnAddWatch = new JButton("Add To Watchlist");
+		btnAddWatch.setBounds(733, 453, 170, 32);
+		btnAddWatch.setFont(new Font("Modern No. 20", Font.BOLD, 14));
+		frame.getContentPane().add(btnAddWatch);
+		
+		lblMoviePoster = new JLabel("Movie Poster");
+		lblMoviePoster.setForeground(Color.BLACK);
+		lblMoviePoster.setBounds(529, 11, 168, 25);
+		lblMoviePoster.setFont(new Font("Cooper Black", Font.BOLD, 20));
+		frame.getContentPane().add(lblMoviePoster);
+		
+		spMovieInfo = new JScrollPane();
+		spMovieInfo.setBounds(723, 39, 180, 276);
+		frame.getContentPane().add(spMovieInfo);
+		
+		txtMovieInfo = new JTextPane();
+		txtMovieInfo.setBackground(new Color(65, 105, 225));
+		txtMovieInfo.setFont(new Font("Tahoma", Font.BOLD, 14));
+		txtMovieInfo.setOpaque(false);
+		txtMovieInfo.setForeground(Color.BLACK);
+		spMovieInfo.setViewportView(txtMovieInfo);
+		spMovieInfo.setOpaque(false);
+		spMovieInfo.getViewport().setOpaque(false);
+		
+		tglbtnUpcoming = new JToggleButton("Upcoming");
+		tglbtnUpcoming.setBounds(196, 0, 100, 32);
+		tglbtnUpcoming.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		frame.getContentPane().add(tglbtnUpcoming);
+		
+		tglbtnPopular = new JToggleButton("Popular");
+		tglbtnPopular.setBounds(0, 0, 100, 32);
+		tglbtnPopular.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		frame.getContentPane().add(tglbtnPopular);
+		
+		tglbtnTopRated = new JToggleButton("Top Rated");
+		tglbtnTopRated.setBounds(99, 0, 100, 32);
+		tglbtnTopRated.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		frame.getContentPane().add(tglbtnTopRated);
+		
+		tglbtnSearch = new JToggleButton("Search");
+		tglbtnSearch.setBounds(393, 0, 100, 32);
+		tglbtnSearch.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		frame.getContentPane().add(tglbtnSearch);
+		
+		tglbtnNowPlaying = new JToggleButton("Now Playing");
+		tglbtnNowPlaying.setBounds(295, 0, 100, 32);
+		tglbtnNowPlaying.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		frame.getContentPane().add(tglbtnNowPlaying);
 		
-		JScrollPane spMovieList = new JScrollPane();
+		
+		spMovieList = new JScrollPane();
 		spMovieList.setBounds(10, 100, 501, 328);
 		frame.getContentPane().add(spMovieList);
 		
@@ -252,6 +338,10 @@ public class GUI {
 				"", "Title", "Release Date", "Rating"
 			}
 		) {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
 			Class[] columnTypes = new Class[] {
 				Integer.class, String.class, String.class, String.class
 			};
@@ -274,16 +364,17 @@ public class GUI {
 		spMovieList.setOpaque(false);
 		spMovieList.getViewport().setOpaque(false);
 		
-		JLabel lblMovieInfo = new JLabel("Movie Info");
+		lblMovieInfo = new JLabel("Movie Info");
 		lblMovieInfo.setForeground(Color.BLACK);
 		lblMovieInfo.setFont(new Font("Cooper Black", Font.BOLD, 20));
 		lblMovieInfo.setBounds(723, 11, 149, 25);
 		frame.getContentPane().add(lblMovieInfo);
 		
-		JLabel lblPlotDiscription = new JLabel("Plot Discription");
+		lblPlotDiscription = new JLabel("Plot Discription");
 		lblPlotDiscription.setForeground(Color.BLACK);
 		lblPlotDiscription.setFont(new Font("Cooper Black", Font.BOLD, 20));
 		lblPlotDiscription.setBounds(527, 316, 186, 25);
 		frame.getContentPane().add(lblPlotDiscription);
 	}
+	
 }

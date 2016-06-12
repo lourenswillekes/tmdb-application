1ef72b9a824930944b36c0805b8479779bd37a41
lourenswillekes <lourw95@gmail.com>
2016-06-12 12:35:18 -0400

    created first version of GuestSession

---
+++ src/source/GuestSession.java
@@ -0,0 +1,85 @@
+package source;
+
+import info.movito.themoviedbapi.model.MovieDb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Class used when the user does not login.
+ * @author Lourens W
+ */
+public class GuestSession implements ISession {
+
+	/**
+	 * Private list of movies for guest favorites.
+	 */
+	private List<MovieDb> guestFavorites;
+	
+	/**
+	 * Private list of movies for guest watchlist.
+	 */
+	private List<MovieDb> guestWatchList;
+	
+	
+	/**
+	 * Public constructor for GuestSession.
+	 */
+	public GuestSession() {
+		guestFavorites = new ArrayList<MovieDb>();
+		guestWatchList = new ArrayList<MovieDb>();
+	}
+	
+	
+	/**
+	 * This method returns the users favorites.
+	 * @return movies on the favorites list
+	 */
+	public final List<MovieDb> getFavorites() {
+		return guestFavorites;
+	}
+
+	/**
+	 * This method returns the users watchlist.
+	 * @return moves on the watchlist list
+	 */
+	public final List<MovieDb> getWatchList() {
+		return guestWatchList;
+	}
+	
+	/**
+	 * This method returns the requested movie from the favorites.
+	 * @param idx the index of the selected movie
+	 * @return the movie that was requested
+	 */
+	public final MovieDb getFavoritesMovie(final int idx) {
+		return guestFavorites.get(idx);
+	}
+	
+	/**
+	 * This method returns the requested movie from the watchlist.
+	 * @param idx the index of the selected movie
+	 * @return the movie that was requested
+	 */
+	public final MovieDb getWatchListMovie(final int idx) {
+		return guestWatchList.get(idx);
+	}
+	
+	/**
+	 * This method removes the requested movie from the favorites.
+	 * @param mov the movie to remove from the list
+	 */
+	public final void rmFavoritesMovie(final MovieDb mov) {
+		guestFavorites.remove(mov);
+	}
+	
+	/**
+	 * This method removes the requested movie from the watchlist.
+	 * @param mov the movie to remove from the list
+	 */
+	public final void rmWatchListMovie(final MovieDb mov) {
+		guestWatchList.remove(mov);
+	}
+	
+}
